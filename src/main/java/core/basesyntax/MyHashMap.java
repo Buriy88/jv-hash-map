50e3e483984cabd7c9b6bd649b3ebee5c2b82d6a
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:07:15 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,18 +2,124 @@ package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Node<K, V>[] table;
+
+    private int size;
+
+    private boolean hasNullKey;
+
+    private V nullValue;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            if (!hasNullKey) {
+                size++;
+                hasNullKey = true;
+            }
+            nullValue = value;
+            return;
+        }
 
+        if ((float) size / table.length >= LOAD_FACTOR) {
+            resize();
+        }
+
+        int index = getIndex(key, table.length);
+        Node<K, V> current = table[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                current.value = value;
+                return;
+            }
+            current = current.next;
+        }
+
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = table[index];
+        table[index] = newNode;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return hasNullKey ? nullValue : null;
+        }
+
+        int index = getIndex(key, table.length);
+        Node<K, V> current = table[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
+
+    private int getIndex(K key, int capacity) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        size = 0;
+
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        K getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return value;
+        }
+
+        void setValue(V value) {
+            this.value = value;
+        }
+
+        Node<K, V> getNext() {
+            return next;
+        }
+
+        void setNext(Node<K, V> next) {
+            this.next = next;
+        }
+    }
+
 }
